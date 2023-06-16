d1bf597d83ca2a04dd06809c4e67764f0f2a332f
Andrew De Forest <andydeforest@gmail.com>
2023-06-16 15:54:32 -0700

    clarified fqdn in verification middleware

--- server/src/verification.middleware.ts
+++ server/src/verification.middleware.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 export function verify(req: Request, res: Response, next: NextFunction) {
-  if (['localhost', 'ntp.guru'].includes(req.hostname)) {
+  if (['localhost', 'https://ntp.guru/'].includes(req.hostname)) {
     next();
   } else {
     throw new HttpException(
